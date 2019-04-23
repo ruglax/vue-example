37d04d652ed6e19f2b136c03d3b588d245d6df5a
Raúl Silva <raul.sr.87@gmail.com>
2019-04-22 20:50:31 -0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,34 @@
+﻿// Allow configure path to chunk files.
+//__webpack_public_path__ = window.resourceBaseUrl;
+
+import Vue from 'vue'
+import Vuex from 'Vuex'
+import ComponentA from './components/common/SelectUsoCFDI.vue'
+
+// Allow vue use vuex for store
+Vue.use(Vuex);
+const store = new Vuex.Store({
+    state: {
+        user: {}
+    },
+    mutations: {
+        login(state, user) {
+            state.user = user;
+        }
+    }
+});
+
+export default {
+  components: {
+    ComponentA
+  },
+}
+
+var vue = new Vue({
+    el: '#app',
+    store,
+    components: {
+        ComponentA
+    }
+});
+            
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+﻿const { VueLoaderPlugin } = require('vue-loader');
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: __dirname + '/dist/bundle/',
+    },
+    resolve: {
+        extensions: [".webpack.js", ".web.js", ".ts", ".vue", ".js", '.css'],
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+        }
+    },
+    externals: {
+        vue: "Vue"
+    },
+    mode: "production",
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    esModule: true
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: "style-loader" },
+                    { loader: "css-loader" }
+                ]
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2)$/,
+                use: "url-loader?name=[name].[ext]"
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ]
+};
